2e11a6f55249b0ff0a16714c67f6c30d981b4b5f
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-05-14 16:51:08 +0800

    2966# add fontawesome

--- microservice-vue/src/main.js
+++ microservice-vue/src/main.js
@@ -9,8 +9,13 @@ import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import moment from 'moment'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText} from '@fortawesome/vue-fontawesome'
 
-
+library.add(fas, far, fab)
 const app = createApp(App)
 
 app.config.globalProperties.$moment = moment
@@ -18,4 +23,7 @@ app.config.globalProperties.$moment = moment
 app.use(ElementPlus, {locale: zhCn})
     .use(store)
     .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .component('font-awesome-layers', FontAwesomeLayers)
+    .component('font-awesome-layers-text', FontAwesomeLayersText)
     .mount('#app')
