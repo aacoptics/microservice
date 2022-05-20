a15eff77f4b8e5db333e348d53cd16941adc0b46
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-05-20 17:53:07 +0800

    2966# eslintrc

--- microservice-vue/.eslintrc.js
+++ microservice-vue/.eslintrc.js
@@ -1,5 +1,15 @@
 // <projectRoot>/.eslintrc.js
 module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
     rules: {
         'vue/multi-word-component-names': 0,
     },
