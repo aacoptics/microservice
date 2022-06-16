5b38548e612bf06bdf7a2457b8d281969d9c386b
60054916 <yanshangqi@aactechnologies.com>
2022-06-16 18:41:16 +0800

    优化消息推送

--- microservice-functional/microservice-notification/src/main/java/com/aacoptics/notification/schedule/NotificationHandle.java
+++ microservice-functional/microservice-notification/src/main/java/com/aacoptics/notification/schedule/NotificationHandle.java
@@ -3,7 +3,6 @@ package com.aacoptics.notification.schedule;
 import com.aacoptics.notification.entity.vo.NotificationEntity;
 import com.aacoptics.notification.service.SendMessageService;
 import com.alibaba.fastjson.JSONObject;
-import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
@@ -19,14 +18,10 @@ public class NotificationHandle {
     SendMessageService sendMessageService;
 
     @XxlJob("NotificationHandle")
-    public ReturnT<String> NotificationHandle(String param) {
+    public void NotificationHandle() throws Exception {
+
+        String param = XxlJobHelper.getJobParam(); //执行参数
         NotificationEntity jobParam = JSONObject.parseObject(param, NotificationEntity.class);
-        try {
-            sendMessageService.sendHandledMessage(jobParam);
-            return ReturnT.SUCCESS;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ReturnT<>(500, e.getMessage());
-        }
+        sendMessageService.sendHandledMessage(jobParam);
     }
 }
