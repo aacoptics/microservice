ce6bc5a16a050de30dd935ad7e1b2e7ada0e3108
60054916 <yanshangqi@aactechnologies.com>
2022-06-20 15:29:34 +0800

    获取人员计划接口

---
+++ microservice-production/microservice-gaia-api/src/main/java/com/aacoptics/gaia/entity/vo/MessageInfo.java
@@ -0,0 +1,24 @@
+package com.aacoptics.gaia.entity.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode()
+@Accessors(chain = true)
+public class MessageInfo implements Serializable {
+
+    private String JobNumber;
+
+    private String Message;
+}
--- microservice-production/microservice-gaia-api/src/main/java/com/aacoptics/gaia/service/impl/PlanActualPerPersonService.java
+++ microservice-production/microservice-gaia-api/src/main/java/com/aacoptics/gaia/service/impl/PlanActualPerPersonService.java
@@ -1,6 +1,8 @@
 package com.aacoptics.gaia.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.aacoptics.gaia.entity.po.PlanActualPerPerson;
+import com.aacoptics.gaia.entity.vo.MessageInfo;
 import com.aacoptics.gaia.mapper.PlanActualPerPersonMapper;
 import com.aacoptics.gaia.service.IPlanActualPerPersonService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -12,7 +14,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,6 +32,7 @@ public class PlanActualPerPersonService extends ServiceImpl<PlanActualPerPersonM
         return planActualPerPersonMapper.getPlanInfoByTime(startDate, endDate);
     }
 
+    @Override
     public void updatePlanResult(List<PlanActualPerPerson> planActualPerPersons){
         for (PlanActualPerPerson planActualPerPerson : planActualPerPersons) {
             UpdateWrapper<PlanActualPerPerson> updateWrapper = new UpdateWrapper<>();
@@ -35,5 +41,76 @@ public class PlanActualPerPersonService extends ServiceImpl<PlanActualPerPersonM
             updateWrapper.set("transfer_err",  planActualPerPerson.getTransferErr());
             baseMapper.update(null, updateWrapper);
         }
+    }
+
+    @Override
+    public List<MessageInfo> getPersonPlanMsg(){
+//        LocalDate previousDate = LocalDate.now().minusDays(1);
+//        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd");
+//        String previousDateStr = previousDate.format(df);
+        List<MessageInfo> messages = new ArrayList<>();
+        List<PlanActualPerPerson> workDateList = getWorkDate();
+        if(workDateList.size() <= 0)
+            return messages;
+        for (PlanActualPerPerson workDate : workDateList) {
+            List<PlanActualPerPerson> needToSendMsg = getDataByWorkDate(workDate.getWorkDate());
+            if(needToSendMsg.size() > 0){
+                String jobNumber = "";
+                StringBuilder message = new StringBuilder();
+                for (PlanActualPerPerson planActualPerPerson : needToSendMsg) {
+                    if(!planActualPerPerson.getEmployeeID().equals(jobNumber)){
+                        if(!StrUtil.isBlank(message.toString())){
+                            MessageInfo messageInfo = new MessageInfo();
+                            messageInfo.setJobNumber(jobNumber);
+                            messageInfo.setMessage(message.toString());
+                            messages.add(messageInfo);
+                            message = new StringBuilder();
+                        }
+                        jobNumber = planActualPerPerson.getEmployeeID();
+                    }
+                    if(StrUtil.isBlank(message.toString())){
+                        message.append(planActualPerPerson.getName())
+                                .append("，您好，")
+                                .append(planActualPerPerson.getWorkDate())
+                                .append("出勤时长").append(planActualPerPerson.getWorkTime()).append("\n")
+                                .append("生产拆分工时").append(planActualPerPerson.getSplitWorkTime())
+                                .append("，你计划生产数量是").append(planActualPerPerson.getPlanDailyCapacityOfProduction().intValue())
+                                .append("，实际生产数量").append(planActualPerPerson.getActualDailyCapacityOfProduction().intValue())
+                                .append("，质量扣除数量").append(planActualPerPerson.getCapacityOfProductionDeduct().intValue())
+                                .append("，综合产能").append(planActualPerPerson.getComprehensiveCapacityOfProduction().intValue()).append("\n");
+                    }
+                    else{
+                        message.append("生产拆分工时").append(planActualPerPerson.getSplitWorkTime())
+                                .append("，你计划生产数量是").append(planActualPerPerson.getPlanDailyCapacityOfProduction().intValue())
+                                .append("，实际生产数量").append(planActualPerPerson.getActualDailyCapacityOfProduction().intValue())
+                                .append("，质量扣除数量").append(planActualPerPerson.getCapacityOfProductionDeduct().intValue())
+                                .append("，综合产能").append(planActualPerPerson.getComprehensiveCapacityOfProduction().intValue()).append("\n");
+                    }
+                }
+                MessageInfo messageInfo = new MessageInfo();
+                messageInfo.setJobNumber(jobNumber);
+                messageInfo.setMessage(message.toString());
+                messages.add(messageInfo);
+            }
+        }
+        return messages;
+    }
+
+    @Override
+    public List<PlanActualPerPerson> getDataByWorkDate(String workDate){
+        QueryWrapper<PlanActualPerPerson> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("work_date", workDate)
+                .eq("dd_transfer_flg", 0)
+                .orderByAsc("EmployeeID");
+        return this.list(queryWrapper);
+    }
+
+    @Override
+    public List<PlanActualPerPerson> getWorkDate(){
+        QueryWrapper<PlanActualPerPerson> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("work_date")
+                .eq("dd_transfer_flg", 0)
+                .groupBy("work_date");
+        return this.list(queryWrapper);
     }
 }
