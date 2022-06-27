dbdc7245f188e7ce5348e27595077e4ac8664048
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-06-27 16:45:17 +0800

    3267# 界面

---
+++ microservice-vue/src/api/lens/quality/qualityOqcPpm.js
@@ -0,0 +1,45 @@
+import request from '@/utils/request'
+
+export function findQualityMilPage(conditions) {
+    return request({
+        url: '/quality-daily/qualityOqcPpm/listQualityOqcPpm',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: conditions
+    })
+}
+
+export function uploadExcel(param) {
+    const formData = new FormData()
+    formData.append('file', param.file)
+    return request({
+        url: '/quality-daily/qualityOqcPpm/uploadExcel',
+        method: 'post',
+        data: formData
+    })
+}
+
+export function exportExcel() {
+    return request({
+        url: '/quality-daily/qualityOqcPpm/exportExcel',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        responseType: 'blob'
+    })
+}
+
+export function listQualityOqcPpmExportExcel(conditions) {
+    return request({
+        url: '/quality-daily/qualityOqcPpm/listQualityOqcPpm/exportExcel',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        responseType: 'blob',
+        data: conditions
+    })
+}
