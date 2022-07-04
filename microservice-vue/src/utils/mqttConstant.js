13bd883a696621a35a786d55c0f5d2127bded219
60054916 <yanshangqi@aactechnologies.com>
2022-07-04 12:19:06 +0800

    修改mqtt客户端配置

--- microservice-vue/src/utils/mqttConstant.js
+++ microservice-vue/src/utils/mqttConstant.js
@@ -1,9 +1,11 @@
 export default {
     MQTT_CONNECTION: {
-        host: 'uds.aacoptics.com',
-        port: 8083,
+        host: 'k8s.aacoptics.com',
+        port: 30931,
         endpoint: '/mqtt',
         clean: true, // 保留会话
+        username: "admin",
+        password: "123456!a",
         connectTimeout: 5000, // 超时时间
         reconnectPeriod: 5000, // 重连时间间隔
     }
