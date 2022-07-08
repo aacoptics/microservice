3be9200e73e73696d3c5b2e1e0f652e9b4027b1b
60054916 <yanshangqi@aactechnologies.com>
2022-07-08 14:57:49 +0800

    添加首页背景

--- microservice-vue/src/views/home/MainPage.vue
+++ microservice-vue/src/views/home/MainPage.vue
@@ -5,16 +5,6 @@
 
 export default {
   name: 'mainPage',
-  mounted() {
-    this.resizeFrame('webFrame')
-  },
-  methods: {
-    resizeFrame(id) {
-      const oIframe = document.getElementById(id);
-      const deviceHeight = document.documentElement.clientHeight;
-      oIframe.style.height = (Number(deviceHeight) - 125) + 'px'; //数字是页面布局高度差
-    }
-  }
 }
 </script>
 
