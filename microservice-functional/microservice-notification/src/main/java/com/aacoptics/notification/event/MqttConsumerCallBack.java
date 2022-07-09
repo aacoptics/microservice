cdba59b3801f63ed6b6e75d226202b8852c3985a
60054916 <yanshangqi@aactechnologies.com>
2022-07-09 10:22:39 +0800

    添加换料提醒

--- microservice-functional/microservice-notification/src/main/java/com/aacoptics/notification/event/MqttConsumerCallBack.java
+++ microservice-functional/microservice-notification/src/main/java/com/aacoptics/notification/event/MqttConsumerCallBack.java
@@ -1,5 +1,6 @@
 package com.aacoptics.notification.event;
 
+import cn.hutool.core.util.StrUtil;
 import com.aacoptics.notification.entity.vo.MarkdownMessage;
 import com.aacoptics.notification.utils.DingTalkUtil;
 import com.alibaba.fastjson.JSONArray;
@@ -7,13 +8,14 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.primitives.Ints;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.*;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 @Slf4j
 
-public class MqttConsumerCallBack implements MqttCallbackExtended{
+public class MqttConsumerCallBack implements MqttCallbackExtended {
 
     private MqttClient client;
 
@@ -36,43 +38,68 @@ public class MqttConsumerCallBack implements MqttCallbackExtended{
     }
 
 
-
     /**
      * 消息到达的回调
      */
     @Override
     public void messageArrived(String topic, MqttMessage message) {
         JSONObject msgJson = JSONObject.parseObject(new String(message.getPayload()));
-        if (msgJson.getString("Message").equals("DoMonitorTempAlarm")) {
-            DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-            //当前时间转换String
-            LocalDateTime time = LocalDateTime.now();
-            String localTimeStr = df.format(time);
-            JSONObject dataJson = msgJson.getJSONObject("Data");
-            MarkdownMessage markdownGroupMessage = new MarkdownMessage();
-            markdownGroupMessage.setTitle("加热棒状态报警");
-            String projectName = dataJson.getString("projectName");
-            String modelName = dataJson.getString("modelName");
-            String param = dataJson.getString("param");
-            String machineName = dataJson.getString("machineName");
-            JSONArray abnormalIdxJson = dataJson.getJSONArray("abnormalIdx");
-            int[] abnormalIdx =  JSONArray.toJavaObject(abnormalIdxJson, int[].class);
-            String abnormalStr = Ints.join(",", abnormalIdx);
-            markdownGroupMessage.addBlobContent(machineName + " " + projectName + " " + modelName);
-            markdownGroupMessage.addBlobContent(localTimeStr);
-            if (param.equals("lower")) {
-                markdownGroupMessage.addContent("下加热床 " + abnormalStr + "号加热棒温度低于平均值5℃。生产人员及时通知设备人员检查加热棒状态，通知工艺人员确定产品性能。");
-            } else {
-                markdownGroupMessage.addContent("上加热床 " + abnormalStr + "号加热棒温度低于平均值5℃。生产人员及时通知设备人员检查加热棒状态，通知工艺人员确定产品性能。");
+        if (StrUtil.isBlank(msgJson.getString("Message")))
+            return;
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        //当前时间转换String
+        LocalDateTime time = LocalDateTime.now();
+        String localTimeStr = df.format(time);
+        JSONObject dataJson = msgJson.getJSONObject("Data");
+        String machineName = dataJson.getString("machineName");
+        MarkdownMessage markdownGroupMessage = new MarkdownMessage();
+        String title = null;
+        switch (msgJson.getString("Message")) {
+            case "DoMonitorTempAlarm":
+                title = "加热棒状态报警";
+                markdownGroupMessage.setTitle(title);
+                String projectName = dataJson.getString("projectName");
+                String modelName = dataJson.getString("modelName");
+                String param = dataJson.getString("param");
+                JSONArray abnormalIdxJson = dataJson.getJSONArray("abnormalIdx");
+                int[] abnormalIdx = JSONArray.toJavaObject(abnormalIdxJson, int[].class);
+                String abnormalStr = Ints.join(",", abnormalIdx);
+                markdownGroupMessage.addBlobContent(machineName + " " + projectName + " " + modelName);
+                markdownGroupMessage.addBlobContent(localTimeStr);
+                if (param.equals("lower")) {
+                    markdownGroupMessage.addContent("下加热床 " + abnormalStr + "号加热棒温度低于平均值5℃。生产人员及时通知设备人员检查加热棒状态，通知工艺人员确定产品性能。");
+                } else {
+                    markdownGroupMessage.addContent("上加热床 " + abnormalStr + "号加热棒温度低于平均值5℃。生产人员及时通知设备人员检查加热棒状态，通知工艺人员确定产品性能。");
+                }
+                String robotUrl = "https://oapi.dingtalk.com/robot/send?access_token=bcf308c4ee97a16d9265365d27001de7f42794d9018702fd253c2d1b28bc442a";
+                try {
+                    Map<String, String> resultMap = DingTalkUtil.sendGroupRobotMessage(robotUrl, "加热棒状态报警", markdownGroupMessage.toString());
+                    JSONObject resultMapJson = (JSONObject) JSONObject.toJSON(resultMap);
+                    log.debug(JSONObject.toJSONString(resultMapJson));
+                } catch (Exception err) {
+                    log.error("发送加热棒报警通知失败！", err);
+                }
+                break;
+            case "FeedAlarm":
+                title = "模造换料提醒";
+                markdownGroupMessage.setTitle(title);
+                markdownGroupMessage.addBlobContent(localTimeStr);
+                markdownGroupMessage.addContent(machineName + "机台需要换料，请相关人员进行处理！");
+                break;
+        }
+
+        String robotUrl = "https://oapi.dingtalk.com/robot/send?access_token=bcf308c4ee97a16d9265365d27001de7f42794d9018702fd253c2d1b28bc442a";
+        try {
+            if(StrUtil.isBlank(title)){
+                log.error("title为空");
+                return;
             }
-            String robotUrl = "https://oapi.dingtalk.com/robot/send?access_token=bcf308c4ee97a16d9265365d27001de7f42794d9018702fd253c2d1b28bc442a";
-            try {
-                Map<String, String> resultMap = DingTalkUtil.sendGroupRobotMessage(robotUrl, "加热棒状态报警", markdownGroupMessage.toString());
-                JSONObject resultMapJson = (JSONObject) JSONObject.toJSON(resultMap);
-                log.debug(JSONObject.toJSONString(resultMapJson));
-            } catch (Exception err) {
-                log.error("发送加热棒报警通知失败！", err);
-            }
+            Map<String, String> resultMap = DingTalkUtil.sendGroupRobotMessage(robotUrl, title, markdownGroupMessage.toString());
+            JSONObject resultMapJson = (JSONObject) JSONObject.toJSON(resultMap);
+            log.debug(JSONObject.toJSONString(resultMapJson));
+        } catch (Exception err) {
+            log.error("发送" + title + "失败！", err);
         }
     }
 
@@ -86,16 +113,19 @@ public class MqttConsumerCallBack implements MqttCallbackExtended{
 
     @Override
     public void connectComplete(boolean b, String s) {
-        if(client.isConnected()) {
+        if (client.isConnected()) {
             log.info("mqtt连接成功");
             //订阅主题
             try {
-                client.subscribe(subTopics, 2);
+                String[] subTopicArray = subTopics.split(",");
+                for (String subTopic : subTopicArray) {
+                    client.subscribe(subTopic, 2);
+                }
                 log.info("mqtt订阅成功");
             } catch (MqttException e) {
                 log.error("mqtt订阅失败", e);
             }
-        }else{
+        } else {
             log.error("mqtt连接失败");
         }
     }
