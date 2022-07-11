6d57f319cf76d53b0147b8f3611144cb5aadc5ef
60054916 <yanshangqi@aactechnologies.com>
2022-07-11 15:24:04 +0800

    添加stompjs

---
+++ microservice-vue/src/utils/rabbitmqConfig.js
@@ -0,0 +1,4 @@
+export const RABBITMQ_SERVICE = 'ws://k8s.aacoptics.com:30903/ws' // mqtt服务地址
+export const RABBITMQ_USERNAME = 'admin' // mqtt连接用户名
+export const RABBITMQ_PASSWORD = 'admin' // mqtt连接密码
+export const RABBITMQ_TOPIC_TOOL_LIFE = '/exchange/aac-optics-mold-toollife-data/mold-toollife-data'//主题
