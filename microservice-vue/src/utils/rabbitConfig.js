7fc455c466efe21d2b1018f17a8e055cbe78345f
60054916 <yanshangqi@aactechnologies.com>
2022-07-12 16:33:04 +0800

    更改rabbitmq地址

--- microservice-vue/src/utils/rabbitConfig.js
+++ microservice-vue/src/utils/rabbitConfig.js
@@ -1,4 +1,4 @@
-export const MQTT_SERVICE = 'ws://k8s.aacoptics.com:30903/ws' // mqtt服务地址
+export const MQTT_SERVICE = 'wss://uds.aacoptics.com:15674/ws' // mqtt服务地址
 export const MQTT_USERNAME = 'admin' // mqtt连接用户名
 export const MQTT_PASSWORD = 'admin' // mqtt连接密码
 export const MQTT_TOPIC_TOOL_LIFE = '/exchange/aac-optics-mold-toollife-data/mold-toollife-data'//主题
