18e907fb68df97dceb9af9707b6c87a042abfb59
60054916 <yanshangqi@aactechnologies.com>
2022-07-21 16:24:47 +0800

    添加FeignConfig

---
+++ microservice-production/iot-mold-toollife/src/main/java/com/aacoptics/mold/toollife/config/FeignConfig.java
@@ -0,0 +1,9 @@
+package com.aacoptics.mold.toollife.config;
+
+import com.aacoptics.common.feign.config.FeignRequestInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig extends FeignRequestInterceptor {
+
+}
