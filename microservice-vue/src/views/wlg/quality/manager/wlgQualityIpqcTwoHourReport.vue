ff65cf3c1977178306ebea24fd9022b93ee05e3d
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-07-31 18:28:14 +0800

    3506# 分页

--- microservice-vue/src/views/wlg/quality/manager/wlgQualityIpqcTwoHourReport.vue
+++ microservice-vue/src/views/wlg/quality/manager/wlgQualityIpqcTwoHourReport.vue
@@ -35,8 +35,6 @@
 
       <SysTable id="condDataTable" ref="sysTable" :columns="columns" :data="pageResult"
                 :height="400" :highlightCurrentRow="true" :show-operation="false" :showBatchDelete="false"
-                :pageSize="100000000" :pageSizes="[100000000]"
-                :showPagination="false"
                 :stripe="false" @findPage="findPage">
       </SysTable>
     </div>
@@ -60,7 +58,7 @@ export default {
         currentDate: date2str(new Date().setDate(new Date().getDate() - 1)),
       },
       columns: [],
-      pageRequest: {current: 1, size: 100000000},
+      pageRequest: {current: 1, size: 10},
       pageResult: {},
       excelUploadDialogVisible: false,
       exportLoading: false,
