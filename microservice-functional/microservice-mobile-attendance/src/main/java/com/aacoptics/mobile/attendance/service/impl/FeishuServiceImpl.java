0c29421f755fd5a899e08725c880a56081bab8a1
60054916 <yanshangqi@aactechnologies.com>
2022-08-23 10:17:47 +0800

    添加byte[] 转 MultipartFile

--- microservice-functional/microservice-mobile-attendance/src/main/java/com/aacoptics/mobile/attendance/service/impl/FeishuServiceImpl.java
+++ microservice-functional/microservice-mobile-attendance/src/main/java/com/aacoptics/mobile/attendance/service/impl/FeishuServiceImpl.java
@@ -3,26 +3,18 @@ package com.aacoptics.mobile.attendance.service.impl;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import com.aacoptics.mobile.attendance.config.FeishuAppKeyConfig;
+import com.aacoptics.mobile.attendance.entity.vo.MockMultipartFile;
 import com.aacoptics.mobile.attendance.exception.BusinessException;
 import com.aacoptics.mobile.attendance.provider.FeishuApiProvider;
 import com.aacoptics.mobile.attendance.service.FeishuService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItem;
-import org.apache.commons.io.IOUtils;
 import org.apache.http.entity.ContentType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.annotation.Resource;
 import java.io.*;
-import java.nio.file.Files;
 
-/**
- * @author Kaizhi Xuan
- * created on 2022/6/25 9:17
- */
 @Service
 @Slf4j
 public class FeishuServiceImpl implements FeishuService {
@@ -46,26 +38,20 @@ public class FeishuServiceImpl implements FeishuService {
     }
 
     @Override
-    public String fetchUploadFileKey(String fileType, String filePath, Integer duration) throws IOException {
+    public String fetchUploadFileKey(String fileType, byte[] fileContent, Integer duration) throws IOException {
         final String accessToken = fetchAccessToken();
         if (StrUtil.isEmpty(accessToken)) return null;
 
-        File file = new File(filePath);
+        InputStream inputStream = new ByteArrayInputStream(fileContent);
+        MultipartFile multipartFile = new MockMultipartFile(ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+        final JSONObject result = feishuApiProvider.fetchUploadFileKey(accessToken, fileType, multipartFile);
+        final String throwable = result.get("Throwable", String.class);
 
+        if (StrUtil.isNotEmpty(throwable))
+            throw new BusinessException(throwable);
 
-
-        try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            FileItem fileItem = new DiskFileItem("file", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
-            IOUtils.copy(fileInputStream, fileItem.getOutputStream());
-            MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
-            final JSONObject result = feishuApiProvider.fetchUploadFileKey(accessToken, fileType, multipartFile);
-            final String throwable = result.get("Throwable", String.class);
-            if (StrUtil.isNotEmpty(throwable))
-                throw new BusinessException(throwable);
-
-            return result.get("code", Integer.class) == 0
-                    ? result.get("data", JSONObject.class).get("file_key", String.class)
-                    : null;
-        }
+        return result.get("code", Integer.class) == 0
+                ? result.get("data", JSONObject.class).get("file_key", String.class)
+                : null;
     }
 }
