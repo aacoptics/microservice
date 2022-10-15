5dc4ecea6b7cd4d294812f2e688a54fa73a86c95
80000061 <PanLei@aacoptics.com>
2022-10-15 16:22:36 +0800

    WLG设备保养工单修改

--- microservice-production/microservice-wlg-equipment/src/main/java/com/aacoptics/wlg/equipment/entity/form/MaintenanceOrderForm.java
+++ microservice-production/microservice-wlg-equipment/src/main/java/com/aacoptics/wlg/equipment/entity/form/MaintenanceOrderForm.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @ApiModel
