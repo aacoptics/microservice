6292257829c4d54e4adbf467e6031ea32666d4e4
KONGXIANGBIAO <KONGXIANGBIAO@AACOPTICS.COM>
2022-10-19 16:40:47 +0800

    新增模流数据

---
+++ microservice-production/microservice-data-analysis-api/src/main/java/com/aacoptics/data/analysis/controller/MoldFlowController.java
@@ -0,0 +1,168 @@
+package com.aacoptics.data.analysis.controller;
+
+import com.aacoptics.common.core.vo.Result;
+import com.aacoptics.data.analysis.entity.form.QueryParams;
+import com.aacoptics.data.analysis.entity.po.MoldFlowData;
+import com.aacoptics.data.analysis.entity.po.StructureData;
+import com.aacoptics.data.analysis.exception.WlgReportErrorType;
+import com.aacoptics.data.analysis.service.IMoldFlowService;
+import com.aacoptics.data.analysis.service.IStructureDataService;
+import com.aacoptics.data.analysis.util.ExcelUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.List;
+
+@RestController
+@RequestMapping("/moldFlow")
+@Api("MoldFlowController")
+@Slf4j
+public class MoldFlowController {
+
+    @Resource
+    IMoldFlowService moldFlowService;
+
+    /**
+     * 导出模板
+     *
+     * @param response
+     */
+    @PostMapping("/exportTemplate")
+    public void downloadStructureDataTemplate(HttpServletResponse response) throws IOException {
+        try {
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("excelTemplate/moldFlow.xlsx");
+            //强制下载不打开
+            response.setContentType("application/force-download");
+            OutputStream out = response.getOutputStream();
+            //使用URLEncoder来防止文件名乱码或者读取错误
+            response.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode("模流数据模板", "UTF-8"));
+            int b = 0;
+            byte[] buffer = new byte[1000000];
+            while (b != -1) {
+                b = inputStream.read(buffer);
+                if (b != -1) {
+                    out.write(buffer, 0, b);
+                }
+            }
+            inputStream.close();
+            out.close();
+            out.flush();
+        } catch (IOException e) {
+            log.error("模板下载异常", e);
+            throw e;
+        }
+    }
+
+    @ApiOperation(value = "模流数据Excel上传", notes = "模流数据Excel上传")
+    @ApiImplicitParam(name = "file", value = "Excel文件", required = true, dataType = "MultipartFile")
+    @PostMapping("/uploadExcel")
+    public Result uploadExcel(@RequestPart("file") MultipartFile file) throws Exception {
+        moldFlowService.importMoldFlowExcel(file.getInputStream());
+        return Result.success();
+    }
+
+    /**
+     * 根据条件查询条件数据
+     *
+     * @param queryParams
+     * @return
+     */
+    @ApiOperation(value = "条件搜索模流数据", notes = "条件搜索模流数据")
+    @PostMapping("/getDataByConditions")
+    public Result getDataByConditions(@RequestBody QueryParams queryParams) {
+        Integer page = queryParams.getCurrent();
+        Integer size = queryParams.getSize();
+        Page<MoldFlowData> iPage = new Page<>(page, size);
+        IPage<MoldFlowData> res = moldFlowService.getDataByConditions(iPage,
+                queryParams.getCategory(),
+                queryParams.getProject(),
+                queryParams.getPartName(),
+                queryParams.getMaterial());
+        if (res.getTotal() == 0) {
+            return Result.fail(WlgReportErrorType.BUSINESS_EXCEPTION, "查询数据为空！");
+        }
+        return Result.success(res);
+    }
+
+
+    @ApiOperation(value = "导出结构数据Excel", notes = "导出结构数据Excel")
+    @PostMapping(value = "/exportExcel")
+    public void exportMoldFlowExcel(@RequestBody QueryParams queryParams, HttpServletResponse response) throws Exception {
+        XSSFWorkbook wb = null;
+        try {
+            // 根据查询条件获取数据
+            List<MoldFlowData> datas = moldFlowService.getAllDataByConditions(queryParams);
+            // 读取模板
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("excelTemplate/moldFlow.xlsx");
+            wb = (XSSFWorkbook) WorkbookFactory.create(inputStream);
+            XSSFSheet sheet = wb.getSheetAt(0);
+
+            if (datas != null && datas.size() > 0) {
+                for (int i = 0; i < datas.size(); i++) {
+                    MoldFlowData p = datas.get(i);
+                    // 获取行
+                    XSSFRow row = sheet.getRow(i + 3);
+                    if (row == null) {
+                        row = sheet.createRow(i + 3);
+                    }
+                    row.createCell(0).setCellValue(p.getCategory());
+                    row.createCell(1).setCellValue(p.getProject());
+                    row.createCell(2).setCellValue(p.getPartName());
+                    row.createCell(3).setCellValue(p.getMaterial());
+                    row.createCell(4).setCellValue(p.getMoldType());
+                    row.createCell(5).setCellValue(p.getMoldDiameterRate());
+                    row.createCell(6).setCellValue(p.getFlowFrontTemperature());
+                    row.createCell(7).setCellValue(p.getVpChangePressure());
+                    row.createCell(8).setCellValue(p.getSimulateWireLength());
+                    row.createCell(9).setCellValue(p.getWholePercent());
+                    row.createCell(10).setCellValue(p.getEffectiveR1());
+                    row.createCell(11).setCellValue(p.getEffectiveR2());
+                    row.createCell(12).setCellValue(p.getRidgeR1());
+                    row.createCell(13).setCellValue(p.getRidgeR2());
+                    row.createCell(14).setCellValue(p.getRefractiveR1());
+                    row.createCell(15).setCellValue(p.getRefractiveR2());
+                    row.createCell(16).setCellValue(p.getCompetitorName());
+                    row.createCell(17).setCellValue(p.getCompetitorLink());
+                    row.createCell(18).setCellValue(p.getAssemblyDrawing());
+                }
+            }
+
+        } catch (Exception e) {
+            log.error("导出数据异常", e);
+            throw e;
+        }
+        ExcelUtil.exportXlsx(response, wb, "模流数据.xlsx");
+    }
+
+
+    @ApiOperation(value = "删除模流数据", notes = "根据url的id来指定删除对象")
+    @ApiImplicitParam(paramType = "path", name = "id", value = "模流数据ID", required = true, dataType = "Long")
+    @DeleteMapping(value = "/delete/{id}")
+    public Result delete(@PathVariable Long id) {
+        return Result.success(moldFlowService.delete(id));
+    }
+
+
+    @ApiOperation(value = "更新结构数据", notes = "修改指定位置的数据")
+    @PostMapping(value = "/update")
+    public Result update(@RequestBody MoldFlowData moldFlowData) {
+        return Result.success(moldFlowService.update(moldFlowData));
+    }
+
+}
---
+++ microservice-vue/src/views/lens/analysis/moldFlow.vue
@@ -0,0 +1,470 @@
+<template>
+  <div>
+    <div class="aac-container">
+      <div class="toolbar w-full" style="float:left;padding-top:10px;padding-left:15px;">
+        <el-form :inline="true" :model="filters" :size="size">
+          <el-row>
+            <el-col :span="4">
+              <el-form-item label="类别" prop="category">
+                <el-input v-model="filters.category" clearable placeholder="类别"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="项目" prop="project">
+                <el-input v-model="filters.project" clearable placeholder="项目"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="零件名称" prop="partName">
+                <el-input v-model="filters.partName" clearable placeholder="零件名称"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="4">
+              <el-form-item label="材料" prop="material">
+                <el-input v-model="filters.material" clearable placeholder="材料"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <el-form :inline="true" :size="size">
+          <el-form-item>
+            <el-button :loading="exportLoading" type="primary" @click="exportExcelTemplate('项目模流数据模板')">导出模板
+              <template #icon>
+                <font-awesome-icon :icon="['fas', 'download']"/>
+              </template>
+            </el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="findPage(null)">查询
+              <template #icon>
+                <font-awesome-icon :icon="['fas', 'magnifying-glass']"/>
+              </template>
+            </el-button>
+          </el-form-item>
+          <el-row align="middle" class="float-right" justify="center">
+            <el-form-item>
+              <el-button size="small" type="info" @click="handleExcelUpload">导入
+                <template #icon>
+                  <font-awesome-icon :icon="['fas', 'upload']"/>
+                </template>
+              </el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-button :loading="exportDataLoading" type="success" @click="exportData('项目模流数据表')">导出数据
+                <template #icon>
+                  <font-awesome-icon :icon="['fas', 'download']"/>
+                </template>
+              </el-button>
+            </el-form-item>
+          </el-row>
+        </el-form>
+      </div>
+
+      <SysTable id="condDataTable" ref="sysTable" :columns="columns" :data="pageResult"
+                :height="400" :highlightCurrentRow="true" :show-operation="false" :showBatchDelete="false"
+                :stripe="false" @findPage="findPage">
+        <template v-slot:custom-column>
+          <el-table-column align="center" fixed="right" header-align="center" label="操作"
+                           width="120">
+            <template v-slot="scope">
+              <el-button-group>
+                <el-tooltip content="编辑" placement="top">
+                  <el-button size="small" type="primary" @click="handleEdit({index: scope.index, row: scope.row})">
+                    <template #icon>
+                      <font-awesome-icon :icon="['far', 'pen-to-square']"/>
+                    </template>
+                  </el-button>
+                </el-tooltip>
+                <el-tooltip content="删除" placement="top">
+                  <el-button size="small" type="danger" @click="handleDelete(scope.row)">
+                    <template #icon>
+                      <font-awesome-icon :icon="['far', 'trash-can']"/>
+                    </template>
+                  </el-button>
+                </el-tooltip>
+              </el-button-group>
+            </template>
+          </el-table-column>
+        </template>
+      </SysTable>
+
+      <el-dialog v-model="excelUploadDialogVisible" :close-on-click-modal="false" :title="'Excel导入'" width="30%">
+        <el-upload
+            :before-upload="beforeUpload"
+            :http-request="submitExcelUpload"
+            :multiple="false"
+            :show-file-list="false"
+            accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            action=""
+            class="upload-demo"
+            drag>
+          <font-awesome-icon :icon="['fas', 'cloud-arrow-up']" size="6x"/>
+          <div class="el-upload__text">将Excel文件拖到此处，或<em>点击上传</em></div>
+        </el-upload>
+        <div class="dialog-footer" style="padding-top: 20px;text-align: end">
+        </div>
+      </el-dialog>
+
+      <el-dialog v-model="editDialogVisible" :close-on-click-modal="false" :title="编辑" width="90%">
+        <el-form ref="dataForm" :model="dataForm" :size="size" label-width="100px">
+          <el-form-item v-if="false" label="Id" prop="id">
+            <el-input v-model="dataForm.id" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="类别" prop="category">
+                <el-input v-model="dataForm.category" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="项目" prop="project">
+                <el-input v-model="dataForm.project" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="零件名称" prop="partName">
+                <el-input v-model="dataForm.partName" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="材料" prop="material">
+                <el-input v-model="dataForm.material" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="模具类型" prop="moldType">
+                <el-input v-model="dataForm.moldType" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="模流外径收缩率(‰)" prop="moldDiameterRate">
+                <el-input v-model="dataForm.moldDiameterRate" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="流动前沿温度(℃)" prop="flowFrontTemperature">
+                <el-input v-model="dataForm.flowFrontTemperature" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="VP切换压力(MPa)" prop="vpChangePressure">
+                <el-input v-model="dataForm.vpChangePressure" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="模拟熔接线长度(mm)" prop="simulateWireLength">
+                <el-input v-model="dataForm.simulateWireLength" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="整体(%)" prop="wholePercent">
+                <el-input v-model="dataForm.wholePercent" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="R1有效径(%)" prop="effectiveR1">
+                <el-input v-model="dataForm.effectiveR1" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="R2有效径(%)" prop="effectiveR2">
+                <el-input v-model="dataForm.effectiveR2" auto-complete="off" clearable
+                          type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="R1坎合(%)" prop="ridgeR1">
+                <el-input v-model="dataForm.ridgeR1" auto-complete="off" clearable
+                          type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="R2坎合(%)" prop="ridgeR2">
+                <el-input v-model="dataForm.ridgeR2" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="R1(nm)" prop="refractiveR1">
+                <el-input v-model="dataForm.refractiveR1" auto-complete="off" clearable
+                          type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="R2(nm)" prop="refractiveR2">
+                <el-input v-model="dataForm.refractiveR2" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="竞品名称" prop="competitorName">
+                <el-input v-model="dataForm.competitorName" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="竞品链接" prop="competitorLink">
+                <el-input v-model="dataForm.competitorLink" auto-complete="off" clearable type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="竞品组立图" prop="assemblyDrawing">
+                <el-input v-model="dataForm.assemblyDrawing" auto-complete="off" clearable
+                          type="textarea"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <div class="dialog-footer" style="padding-top: 20px;text-align: end">
+          <slot name="footer">
+            <el-button :size="size" @click="cancel">取消</el-button>
+            <el-button :loading="editLoading" :size="size" type="primary" @click="submitForm">提交</el-button>
+          </slot>
+        </div>
+      </el-dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+import {getResponseDataMessage} from "@/utils/commonUtils";
+import SysTable from "@/components/SysTable";
+import {
+  exportExcel,
+  exportExcelTemplate,
+  getDataByConditions,
+  handleDelete,
+  handleUpdate,
+  uploadExcel
+} from "@/api/lens/analysis/moldFlow";
+import {ElMessageBox} from "element-plus";
+
+export default {
+  name: "moldFlow",
+  components: {SysTable},
+  data() {
+    return {
+      size: "default",
+      filters: {
+        category: "",
+        project: "",
+        partName: "",
+        material: ""
+      },
+      columns: [
+        {type: "index", label: "序号", minWidth: 50},
+        {prop: "category", label: "类别", minWidth: 100},
+        {prop: "project", label: "项目", minWidth: 100},
+        {prop: "partName", label: "零件名称", minWidth: 100},
+        {prop: "material", label: "材料", minWidth: 100},
+        {prop: "moldType", label: "模具类型", minWidth: 100},
+        {prop: "moldDiameterRate", label: "模流外径收缩率(‰)", minWidth: 100},
+        {prop: "flowFrontTemperature", label: "流动前沿温度(℃)", minWidth: 100},
+        {prop: "vpChangePressure", label: "VP切换压力(MPa)", minWidth: 100},
+        {prop: "simulateWireLength", label: "模拟熔接线长度(mm)", minWidth: 100},
+        {prop: "wholePercent", label: "整体(%)", minWidth: 100},
+        {prop: "effectiveR1", label: "R1有效径(%)", minWidth: 100},
+        {prop: "effectiveR2", label: "R2有效径(%)", minWidth: 100},
+        {prop: "ridgeR1", label: "R1坎合(%)", minWidth: 100},
+        {prop: "ridgeR2", label: "R2坎合(%)", minWidth: 100},
+        {prop: "refractiveR1", label: "R1(nm)", minWidth: 100},
+        {prop: "refractiveR2", label: "R2(nm)", minWidth: 100},
+        {prop: "competitorName", label: "竞品名称", minWidth: 100},
+        {prop: "competitorLink", label: "竞品链接", minWidth: 100},
+        {prop: "assemblyDrawing", label: "竞品组立图", minWidth: 100}
+      ],
+      pageRequest: {current: 1, size: 10},
+      pageResult: {},
+      editDialogVisible: false, // 编辑界面是否显示
+      editLoading: false,
+      excelUploadDialogVisible: false,
+      exportLoading: false,
+      exportDataLoading: false,
+      // 编辑界面数据
+      dataForm: {
+        id: "0",
+        category: "",
+        project: "",
+        partName: "",
+        material: "",
+        moldType: "",
+        moldDiameterRate: "",
+        flowFrontTemperature: "",
+        vpChangePressure: "",
+        simulateWireLength: "",
+        wholePercent: "",
+        effectiveR1: "",
+        effectiveR2: "",
+        ridgeR1: "",
+        ridgeR2: "",
+        refractiveR1: "",
+        refractiveR2: "",
+        competitorName: "",
+        competitorLink: "",
+        assemblyDrawing: "",
+        createdBy: "",
+        updatedBy: "",
+        createdTime: "",
+        updatedTime: ""
+      },
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    // 获取分页数据
+    findPage: function (data) {
+      if (data !== null) {
+        this.pageRequest = data.pageRequest;
+      }
+      this.pageRequest.category = this.filters.category;
+      this.pageRequest.project = this.filters.project;
+      this.pageRequest.partName = this.filters.partName;
+      this.pageRequest.material = this.filters.material;
+      getDataByConditions(this.pageRequest)
+          .then((res) => {
+            const responseData = res.data;
+            if (responseData.code === "000000") {
+              this.pageResult = responseData.data;
+            } else {
+              this.$message({message: getResponseDataMessage(responseData), type: "error"});
+            }
+          })
+          .then(data != null ? data.callback : "");
+    },
+
+    // 显示编辑界面
+    handleEdit: function (params) {
+      this.editDialogVisible = true;
+      this.dataForm = Object.assign({}, params.row);
+    },
+
+    // 编辑提交
+    submitForm: function () {
+      this.$confirm("确认提交吗？", "提示", {}).then(() => {
+        this.editLoading = true;
+        let params = Object.assign({}, this.dataForm);
+        handleUpdate(params).then((res) => {
+          const responseData = res.data;
+          this.editLoading = false;
+          if (responseData.code === "000000") {
+            this.$message({message: "操作成功", type: "success"});
+            this.editDialogVisible = false;
+            this.$refs["dataForm"].resetFields();
+          } else {
+            this.$message({message: "操作失败" + getResponseDataMessage(responseData), type: "error",});
+          }
+          this.findPage(null);
+        })
+      })
+    },
+
+    // 打开excel导入对话框
+    handleExcelUpload: function () {
+      this.excelUploadDialogVisible = true
+    },
+    beforeUpload(file) {
+      const isExcel = file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' || file.type === 'application/vnd.ms-excel'
+      if (!isExcel) {
+        this.$message.error('只能上传xlsx, xls格式的文件！')
+        return false
+      }
+    },
+    submitExcelUpload(params) {
+      uploadExcel(params).then((response) => {
+        const responseData = response.data
+        if (responseData.code === '000000') {
+          ElMessageBox.alert('上传成功！' + getResponseDataMessage(responseData, '</br>'), '上传信息', {
+            dangerouslyUseHTMLString: true
+          })
+        } else {
+          ElMessageBox.alert('上传失败！' + getResponseDataMessage(responseData, '</br>'), '上传信息', {
+            dangerouslyUseHTMLString: true
+          })
+        }
+        this.findPage(null);
+        this.excelUploadDialogVisible = false;
+      }).catch((err) => {
+        ElMessageBox.alert(err, '上传信息', {
+          dangerouslyUseHTMLString: true,
+          type: 'error'
+        })
+        this.excelUploadDialogVisible = false;
+      })
+    },
+
+    // 导出模板
+    exportExcelTemplate(excelFileName) {
+      this.exportLoading = true;
+      exportExcelTemplate().then(res => {
+        this.exportLoading = false;
+        let url = window.URL.createObjectURL(new Blob([res.data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}));
+        let link = document.createElement('a');
+        link.style.display = 'none';
+        link.href = url;
+        link.setAttribute('download', excelFileName + ".xlsx");
+        document.body.appendChild(link);
+        link.click();
+      })
+    },
+
+    // 导出查询数据
+    exportData(excelFileName) {
+      this.pageRequest.category = this.filters.category;
+      this.pageRequest.project = this.filters.project;
+      this.pageRequest.partName = this.filters.partName;
+      this.pageRequest.material = this.filters.material;
+
+      this.exportDataLoading = true;
+      exportExcel(this.pageRequest).then(res => {
+        this.exportDataLoading = false;
+        let url = window.URL.createObjectURL(new Blob([res.data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}));
+        let link = document.createElement('a');
+        link.style.display = 'none';
+        link.href = url;
+        link.setAttribute('download', excelFileName + "-" + new Date().getTime() + ".xlsx");
+        document.body.appendChild(link);
+        link.click();
+      });
+    },
+
+    // 取消
+    cancel() {
+      this.dialogVisible = false;
+    },
+
+    // 批量删除
+    handleDelete: function (data) {
+      this.$confirm('确认删除选中记录吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        handleDelete(data).then(res => {
+          const responseData = res.data
+          if (responseData.code === '000000') {
+            this.findPage(null);
+            this.$message({message: '删除成功', type: 'success'})
+          } else {
+            this.$message({
+              message: `操作失败${getResponseDataMessage(responseData)}`,
+              type: 'error'
+            })
+          }
+        });
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+};
+</script>
