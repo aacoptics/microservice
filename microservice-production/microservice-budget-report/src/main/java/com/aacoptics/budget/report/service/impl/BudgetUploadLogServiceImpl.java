9579e9db3c27de1d2871c6dbae34d689f03b54de
80000061 <PanLei@aacoptics.com>
2022-10-21 08:32:53 +0800

    财务预算修改

--- microservice-production/microservice-budget-report/src/main/java/com/aacoptics/budget/report/service/impl/BudgetUploadLogServiceImpl.java
+++ microservice-production/microservice-budget-report/src/main/java/com/aacoptics/budget/report/service/impl/BudgetUploadLogServiceImpl.java
@@ -1,11 +1,19 @@
 package com.aacoptics.budget.report.service.impl;
 
 
+import com.aacoptics.budget.report.constants.BudgetTypeConstants;
 import com.aacoptics.budget.report.entity.param.BudgetUploadLogQueryParam;
 import com.aacoptics.budget.report.entity.po.BudgetUploadLog;
+import com.aacoptics.budget.report.entity.po.ProductLineBudget;
+import com.aacoptics.budget.report.entity.po.ProductionCostBudget;
+import com.aacoptics.budget.report.entity.po.ResearchBudget;
 import com.aacoptics.budget.report.exception.BusinessException;
 import com.aacoptics.budget.report.mapper.BudgetUploadLogMapper;
+import com.aacoptics.budget.report.mapper.ProductLineBudgetMapper;
+import com.aacoptics.budget.report.mapper.ProductionCostBudgetMapper;
+import com.aacoptics.budget.report.mapper.ResearchBudgetMapper;
 import com.aacoptics.budget.report.service.BudgetUploadLogService;
+import com.aacoptics.budget.report.service.ResearchBudgetService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -13,14 +21,23 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.Objects;
 
 @Service
 @Slf4j
 public class BudgetUploadLogServiceImpl extends ServiceImpl<BudgetUploadLogMapper, BudgetUploadLog> implements BudgetUploadLogService {
 
+    @Resource
+    ResearchBudgetMapper researchBudgetMapper;
 
+    @Resource
+    ProductLineBudgetMapper productLineBudgetMapper;
+
+    @Resource
+    ProductionCostBudgetMapper productionCostBudgetMapper;
 
     @Override
     public IPage<BudgetUploadLog> query(Page page, BudgetUploadLogQueryParam budgetUploadLogQueryParam) {
@@ -41,8 +58,28 @@ public class BudgetUploadLogServiceImpl extends ServiceImpl<BudgetUploadLogMappe
         return isSuccess;
     }
 
+    @Transactional
     @Override
     public boolean delete(Long id) {
+        BudgetUploadLog budgetUploadLog = this.get(id);
+        if(BudgetTypeConstants.RESEARCH_BUDGET.equals(budgetUploadLog.getType()))
+        {
+            QueryWrapper<ResearchBudget> researchBudgetQueryWrapper = new QueryWrapper<>();
+            researchBudgetQueryWrapper.eq("upload_log_id", id);
+            researchBudgetMapper.delete(researchBudgetQueryWrapper);
+        }
+        else if(BudgetTypeConstants.PRODUCTION_COST_BUDGET.equals(budgetUploadLog.getType()))
+        {
+            QueryWrapper<ProductionCostBudget> productionCostBudgetQueryWrapper = new QueryWrapper<>();
+            productionCostBudgetQueryWrapper.eq("upload_log_id", id);
+            productionCostBudgetMapper.delete(productionCostBudgetQueryWrapper);
+        }
+        else if(BudgetTypeConstants.PRODUCT_LINE_BUDGET.equals(budgetUploadLog.getType()))
+        {
+            QueryWrapper<ProductLineBudget> productLineBudgetQueryWrapper = new QueryWrapper<>();
+            productLineBudgetQueryWrapper.eq("upload_log_id", id);
+            productLineBudgetMapper.delete(productLineBudgetQueryWrapper);
+        }
         return this.removeById(id);
     }
 
