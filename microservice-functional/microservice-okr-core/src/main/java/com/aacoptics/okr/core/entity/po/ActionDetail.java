fb0cfe3a2cf54f2b8256e35ca2228448feba2d1d
60054916 <yanshangqi@aactechnologies.com>
2022-12-01 10:25:23 +0800

    bugfix

--- microservice-functional/microservice-okr-core/src/main/java/com/aacoptics/okr/core/entity/po/ActionDetail.java
+++ microservice-functional/microservice-okr-core/src/main/java/com/aacoptics/okr/core/entity/po/ActionDetail.java
@@ -28,7 +28,7 @@ public class ActionDetail implements Serializable {
     private String actionName;
     private String actionType;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dueTime;
     private String supportDetail;
     private String atUsers;
