07f9bf9ad7b3b5b9cc4b08c174d6d1eaad7b0cc3
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-12-07 14:39:45 +0800

    4169# http地址转义

--- microservice-functional/microservice-okr-core/src/main/java/com/aacoptics/okr/core/service/impl/ObjectiveDetailServiceImpl.java
+++ microservice-functional/microservice-okr-core/src/main/java/com/aacoptics/okr/core/service/impl/ObjectiveDetailServiceImpl.java
@@ -213,7 +213,7 @@ public class ObjectiveDetailServiceImpl extends ServiceImpl<ObjectiveDetailMappe
         markdownGroupMessage.addContent("Objective内容：" + objectiveDetail.getObjectiveName());
         String atUsers = objectiveDetail.getUsers().stream().map(FeishuUser::getName).collect(Collectors.joining(","));
         markdownGroupMessage.addContent("提及人员：" + atUsers);
-        markdownGroupMessage.addContent(StrUtil.format("[查看详情](https://open.feishu.cn/open-apis/authen/v1/index?app_id=cli_a3f634b596a3100c&redirect_uri=http://udsapi.aacoptics.com/okrAtUser?username={}&objectiveDetailId={})",
+        markdownGroupMessage.addContent(StrUtil.format("[查看详情](https://open.feishu.cn/open-apis/authen/v1/index?app_id=cli_a3f634b596a3100c&redirect_uri=http%3A%2F%2Fudsapi.aacoptics.com%2FokrAtUser%3Fusername%3D{}%26objectiveDetailId%3D{})",
                 objectiveDetail.getCreatedBy(), objectiveDetail.getId()));
         return markdownGroupMessage.toString();
     }
