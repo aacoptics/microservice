4811d9af37c5babcb0dac6ee2bd9558cd872f798
80000061 <PanLei@aacoptics.com>
2023-01-10 17:50:01 +0800

    WLG设备状态接口开发

---
+++ microservice-production/wlg-iot-dashboard/src/main/java/com/aacoptics/wlg/dashboard/mapper/MoldingMachineStatusDataMapper.java
@@ -0,0 +1,38 @@
+package com.aacoptics.wlg.dashboard.mapper;
+
+
+import com.aacoptics.wlg.dashboard.entity.po.MoldingMachineStatusData;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@Mapper
+public interface MoldingMachineStatusDataMapper extends BaseMapper<MoldingMachineStatusData> {
+
+
+    /**
+     * 获取机台当前状态
+     * @param machineName
+     * @return
+     */
+    MoldingMachineStatusData getTopOneMachineStatusData(@Param("machineName") String machineName);
+
+
+    /**
+     * 获取ACTIVATED 总时长
+     * @param machineName
+     * @return
+     */
+    Long getMachineActivatedTime(@Param("machineName") String machineName, @Param("startTime") LocalDateTime startTime);
+
+    /**
+     * 统计设备状态数量
+     */
+    List<Map<String, Object>> getMachineStatusSummary(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
+}
