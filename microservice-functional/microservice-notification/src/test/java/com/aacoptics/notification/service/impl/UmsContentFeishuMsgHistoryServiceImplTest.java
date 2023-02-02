3a45b54e08ffc74d34f393460610bb50d831d614
60054916 <yanshangqi@aactechnologies.com>
2023-02-02 15:19:15 +0800

    添加测试

---
+++ microservice-functional/microservice-notification/src/test/java/com/aacoptics/notification/service/impl/UmsContentFeishuMsgHistoryServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.aacoptics.notification.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UmsContentFeishuMsgHistoryServiceImplTest {
+
+    @Test
+    void deleteMessageByBatchId() {
+    }
+}
